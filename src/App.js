e01523eb0cb8912e821f22acd23aaf68c63e4519
annaboyatyuk <annaboyatyuk@gmail.com>
2018-09-17 20:16:09 -0700

    dashboard shows h1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
 import './App.css';
+import Dashboard from './components/Dashboard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <Route exact path='/' component={Dashboard} />
+      </BrowserRouter>
     );
   }
 }
