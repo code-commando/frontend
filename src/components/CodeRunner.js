6aeef1262af38447e215d0efbebe4acd53da00d7
Richard Tae <taerichard@yahoo.com>
2018-09-17 20:47:55 -0700

    yo

--- src/components/CodeRunner.js
+++ src/components/CodeRunner.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class CodeRunner extends Component {
   render() {
     return (
-      <h1>Code runner</h1>
+      <div>
+        <h1>Code runner</h1>
+        <h1>hi</h1>
+      </div>
     );
   }
 }
