68c0e08065832af525f9917b42413b6bc22d2b4b
annaboyatyuk <annaboyatyuk@gmail.com>
2018-09-18 21:31:18 -0700

    add redux store and reducer and action for roster

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -3,12 +3,12 @@ import React, { Component, Fragment } from 'react';
 export default class Courses extends Component {
   render() {
     return (
-      <React.Fragment>
-      <header>
-        <h1>Username Classes</h1>
-      </header>
-      <h1>Courses</h1>
-      </React.Fragment>
+      <Fragment>
+        <header>
+          <h1>Username Classes</h1>
+        </header>
+        <h1>Courses</h1>
+      </Fragment>
 
     );
   }
