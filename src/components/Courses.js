e59cd001a474daa9121bed22e2bc947584d6053e
James McDaniel <icathaid@gmail.com>
2018-09-19 02:20:13 -0700

    added styling to courses.js

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,18 +1,70 @@
 import React, { Component, Fragment } from 'react';
 
-const coursesStyle = {
-  background: 'red',
-  color: 'blue',
+//  i just put this in to help position other elements, navbar should contain its own styling
+const headerStyle = {
+  borderWidth: '10px',
+  borderColor: 'black',
+  backgroundColor: '#D23833',
+  color: 'black',
+  height: '10vh',
+  width: '100%',
+  textAlign: 'center',
 };
 
+const main = {
+  padding: '5em',
+  textAlign: 'center',
+  background: '#D6D6D6',
+  height: '65vh',
+};
+const coursesStyle = {
+  display: 'inline-block',
+  background: '#D23833',
+  color: 'white',
+  height: '35vh',
+  width: 'auto',
+  marginLeft: '15vh',
+  marginRight: '15vh',
+};
+
+const titleStyle = {
+  color: 'black',
+  fontSize: '3em',
+  textAlign: 'center',
+  marginBottom: '10vh',
+};
+
+const textStyle = {
+  textAlign: 'center',
+  fontSize: '3em',
+  paddingTop: '4vh',
+  paddingLeft: '10vh',
+  paddingRight: '10vh',
+};
+
+const imageStyle = {
+  textAlign: 'right',
+};
+
+//  i couldn't remember how to get divs to render as inline-block so i made them navs for now
 export default class Courses extends Component {
   render() {
     return (
       <Fragment>
-        <header style={coursesStyle}>
-          <h1>Username Classes</h1>
+        <header style={headerStyle}>
+          Placeholder for Navbar
         </header>
-        <h1 style={coursesStyle}>Courses</h1>
+        <div style={main}>
+          <h1 style={titleStyle}>Welcome [user]</h1>
+          <nav style={coursesStyle}>
+            <p style={imageStyle}>placeholder for image</p>
+            <p style={textStyle}>301d##</p>
+          </nav>
+          <nav style={coursesStyle}>
+            <p style={imageStyle}>placeholder for image</p>
+            <p style={textStyle}>401n##</p>
+          </nav>
+        </div>
       </Fragment>
     );
   }
