19ae2bcac586a03e19220e599532df56d351e112
annaboyatyuk <annaboyatyuk@gmail.com>
2018-09-19 19:27:46 -0700

    add login with github on homepage

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rosterReducer from '../reducer/roster-reducer.js';
-import signinReducer from '../reducer/signin-reducer.js';
 
 import logger from '../middleware/logger.js';
 
 const appReducer = combineReducers({
-  rosterReducer, signinReducer,
+  rosterReducer,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
