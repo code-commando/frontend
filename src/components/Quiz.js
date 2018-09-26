c52dbb19f265e1ed89f47ead2687ee74faf5fdd4
James McDaniel <icathaid@gmail.com>
2018-09-25 18:50:28 -0700

    stash this

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,11 +5,26 @@ import HeaderBar from './HeaderBar.js';
 import QuizData from './QuizData.js';
 
 const main = {
+  display: 'inline-block',
   background: '#D6D6D6',
   minHeight: '100vh',
   height: '100%',
   width: '100%',
   textAlign: 'center',
+};
+
+const quizStyle = {
+  display: 'inline-block',
+  borderStyle: 'solid',
+  borderWidth: '5px',
+  borderColor: 'blue',
+  height: '80%',
+  width: '80%',
+};
+
+const buttonStyle = {
+  display: 'block',
+  margin: 'auto',
 };
 
 class Quiz extends Component {
@@ -18,21 +33,21 @@ class Quiz extends Component {
   }
 
   renderQuiz = () => {
-    this.setState({ showQuiz: true});
+    this.setState({ showQuiz: true });
   }
 
   render() {
     return (
       <Fragment>
-        <HeaderBar />
-        <NavBar />
-        <h1>Quiz</h1>
-        <p>display questions</p>
-        <p>display answers</p>
-        <button onClick={this.renderQuiz}>Generate Quiz</button>
-        <ul>
-          {this.state.showQuiz && < QuizData/>}
-        </ul>
+        <div style={main}>
+          <HeaderBar />
+          <NavBar />
+          <h1>Quiz</h1>
+          <button style={buttonStyle} onClick={this.renderQuiz}>Generate Quiz</button>
+          <ul style={quizStyle}>
+            {this.state.showQuiz && < QuizData />}
+          </ul>
+        </div>
       </Fragment>
     );
   }
