4e9e935fad6af126753825a2959ef183dcf0f502
Richard Tae <taerichard@yahoo.com>
2018-09-28 21:21:33 -0700

    connected to api, output value shows, just redesigning layout

--- src/components/CodeRunner.js
+++ src/components/CodeRunner.js
@@ -1,139 +1,136 @@
 import React, { Component, Fragment } from 'react';
-
-
+import superagent from 'superagent';
 import style from '../style/style.js';
 import cookies from 'react-cookies';
-import {login} from '../actions/login-action.js';
-import {Redirect} from 'react-router-dom';
-import {connect} from 'react-redux';
-
-
+import { login } from '../actions/login-action.js';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 import NavBar from './NavBar';
 import HeaderBar from './HeaderBar';
 import MonacoEditor from 'react-monaco-editor';
 
-// const main = {
-//   background: '#D6D6D6',
-//   minHeight: '100vh',
-//   height: '100%',
-//   width: '100%',
-//   textAlign: 'center',
-// };
-// const editorFormat = {
-//   textAlign: 'left',
-// };
 class CodeRunner extends Component {
   constructor(props) {
     super(props);
     this.state = {
       code: '',
+      codeResult: '',
       options: {},
     };
+
+    this.onChange = this.onChange.bind(this);
+    this.handleOnClick = this.handleOnClick.bind(this);
   }
+
   editorWillMount(editor, monaco) {
     console.log('editor mounnted', editor, monaco);
-    // const model = this.refs.monaco.editor.getModel();
-    // const value = model.getValue();
-    // console.log('value', value);
-    // console.log(editor);
-    //   monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
-    //     schemas: [
-    //       {
-    //         uri: 'http://myserver/foo-schema.json',
-    //         schema: {
-    //           type: 'object',
-    //           properties: {
-    //             p1: {
-    //               enum: ['v1', 'v2'],
-    //             },
-    //             p2: {
-    //               $ref: 'http://myserver/bar-schema.json',
-    //             },
-    //           },
-    //         },
-    //       },
-    //     ],
-    //   });
-    // }
   }
   editorDidMount(editor, monaco) {
     console.log('editorDidMount', editor);
+    console.log(monaco);
     editor.focus();
   }
-  onChange(newValue, e) {
-    console.log('onChange', newValue, e);
+  onChange(code) {
+    console.log('target', code);
+    this.setState({
+      code,
+    });
   }
 
-  // editorDidMount(editor, monaco) {
-  //   // const model = this.refs.monaco.editor.getModel();
-  //   console.log('monaco', monaco);
-  //   console.log('editorDidMount', editor);
-  //   editor.focus();
-
-  //   //
-  // }
-  // onChange(event) {
-  //   // let newState = { ...this.state.code };
-  //   // newState[event.target.name] = event.target.value;
-  //   // this.setState(newState);
-  //   // this.setState({ code: event.target.value });
-  //   console.log('onChange', event);
-  // }
   handleOnClick(event) {
     event.preventDefault();
     console.log('run code');
+    const token = cookies.load('token');
+    const classCode = '401n5';
+    let obj = {};
+    obj.code = this.state.code;
+    obj.day = 2;
+    obj.language = 'javascript';
+    obj.event = 'run';
+    superagent
+      .post(`http://api.commando.ccs.net/api/v1/code?classCode=${classCode}`)
+      .auth(token, { type: 'bearer' })
+      .send(obj)
+      .then(data => {
+        console.log('this is the body', data.body);
+        let codeResult = '';
+        if (data.body.error) {
+          if (obj.language === 'javascript') {
+            console.log('data.body.error1', data.body.error);
+            codeResult = JSON.stringify(data.body.error);
+          } else {
+            codeResult = data.body.error;
+            console.log('data.body.error2', data.body.error);
+          }
+        } else if (data.body.return && !data.body.log) {
+          codeResult = data.body.return;
+        } else {
+          codeResult = data.body.output;
+        }
+        this.setState({ codeResult });
+      });
   }
 
-  // // const code = this.state.code;
-  // // const options = {
-  // //   selectOnLineNumbers: true,
-  // const makeNeat = {
-  //   textAlign: 'left',
-  
-
-  
-  
   componentDidMount() {
     document.title = 'Code Runner';
-    if(cookies.load('token')) {
+    if (cookies.load('token')) {
       this.props.login();
     }
   }
 
   render() {
-    const code = this.state.code;
-    const options = {
-      selectOnLineNumbers: true,
+    const makeWrapFly = {
+      width: '485px',
     };
-    
-    if(cookies.load('token')) {
+    const makeLeftFly = {
+      width: '240px',
+      height: '123px',
+      float: 'left',
+    };
+
+    const makeRightFly = {
+      width: '240px',
+      height: '123px',
+      float: 'right',
+    };
+
+    // // const code = this.state.code;
+    // // const options = {
+    // //   selectOnLineNumbers:, true
+
+    if (cookies.load('token')) {
+      // console.log(cookies.load('token'));
       return (
         <Fragment>
-
           <style.NavBar />
 
-          <div>
-            <h1>Code runner</h1>
-            <MonacoEditor
-              ref="monaco"
-              // style={makeNeat}
-              width="500"
-              height="500"
-              language="javascript"
-              theme="vs-dark"
-              value={this.state.code}
-              onChange={this.onChange.bind(this)}
-              editorDidMount={this.editorDidMount.bind(this)}
-              editorWillMount={this.editorWillMount.bind(this)}
-            />
-            <button onClick={this.handleOnClick}>run code</button>
+          <HeaderBar />
+          <NavBar />
+          <h1>Code runner</h1>
+          <div id="wrap" style={makeWrapFly}>
+            <div className="left" style={makeLeftFly}>
+              <MonacoEditor
+                name="code"
+                ref="monaco"
+                width="200"
+                height="200"
+                language="javascript"
+                theme="vs-dark"
+                value={this.state.code}
+                onChange={this.onChange}
+                editorDidMount={this.editorDidMount.bind(this)}
+                editorWillMount={this.editorWillMount.bind(this)}
+              />
+              <button onClick={this.handleOnClick}>run code</button>
+            </div>
+            <div className="right" style={makeRightFly}>
+              <textarea className="output" value={this.state.codeResult} />
+            </div>
           </div>
         </Fragment>
       );
-    }
-    else {
-      return <Redirect to='/signin'/>;
-
+    } else {
+      return <Redirect to="/signin" />;
     }
   }
 }
@@ -144,30 +141,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = { login };
 
-export default connect(mapStateToProps, mapDispatchToProps)(CodeRunner);
-
-// handleClick = event => {
-//   event.preventDefault();
-//   let githubURL = 'https://github.com/login/oauth/authorize';
-
-//   let options = {
-//     // local
-//     client_id: 'd6c0defbd80f3979493a',
-//     //live
-//     // client_id: 'f749977a8455b627dc56',
-//     redirect_uri: 'http://localhost:3000/oauth',
-//     // redirect_uri: 'https://code-commando.herokuapp.com/oauth',
-//     scope: 'read:user repo',
-//     state: 'autumn',
-//     allow_signup: 'true',
-//   };
-//   let QueryString = Object.keys(options)
-//     .map(key => {
-//       return `${key}=` + encodeURIComponent(options[key]);
-//     })
-//     .join('&');
-
-//   let authURL = `${githubURL}?${QueryString}`;
-
-//   window.location = authURL;
-// };
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CodeRunner);
